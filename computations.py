675af0511fd09275c7f8eda13e44c756d43eff45
Darco Murja <darcomurja@gmail.com>
2020-04-20 18:20:35 +0300

    lowestReview

--- computations.py
+++ computations.py
@@ -1,6 +1,15 @@
 def getHighestRating(reviewData, category):
     max = 0
+
     for i in reviewData:
         if float(i[category]) > max:
             max = float(i[category])
     return max
+
+
+def getLowestRating(reviewData, category):
+    min = 5
+    for i in reviewData:
+        if float(i[category]) <= min:
+            min = float(i[category])
+    return min
--- plots.py
+++ plots.py
@@ -18,6 +18,17 @@ def plotHighestReviews(reviewData):
     plt.show()
 
 
+def plotLowestReviews(reviewData):
+    reviewDataKeys = [key for key in reviewData[0].keys()]
+    reviewDataKeys = reviewDataKeys[1:]
+    lowestReviews = dict()
 
+    for key in reviewDataKeys:
+        lowestReviews[key] = computations.getLowestRating(reviewData, key)
 
-
+    lowestReviewsSorted = {k: v for k, v in sorted(lowestReviews.items(), key=lambda item: item[1])}
+    print(lowestReviewsSorted)
+    plt.barh([x for x in lowestReviewsSorted.keys()], [x for x in lowestReviewsSorted.values()])
+    plt.title("Lowest rating of each category")
+    plt.tight_layout()
+    plt.show()
